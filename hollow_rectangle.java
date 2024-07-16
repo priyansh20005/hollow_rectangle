b6cde5bfb6d69e666ac794ec62f478c565147ae3
priyansh koshti <priyansh.koshti.cs23@ggits.net>
2024-07-16 22:54:26 +0530

    better way to print

--- hollow_rectangle.java
+++ hollow_rectangle.java
@@ -4,24 +4,36 @@
 
     public static void createHollowRectangle(int rows , int columns){
 
-        for (int i = 1 ;i<= rows ;i++ ){
-
-            if( i == 1 || i == rows){
-                for(int j = 1 ; j<= columns ;j++){
+        for (int i = 1 ;i <= rows ; i++){
+            for (int j = 1 ; j <= columns ; j++){
+                if (i == 1 || i == rows || j== 1 || j == columns){
                     System.out.print("# ");
-                }
-           }else{
-                for(int j = 1 ; j<= rows ;j++){
-                    if (j ==1 || j == columns){
-                        System.out.print("# ");
-                  }else{
+                }else{
                     System.out.print("  ");
-                         }
-                                                }
-                   }
-            System.out.println("");
-
+                }
+            }
+            System.out.println();
         }
+
+/////////////////////////////////// other way//////////
+        // for (int i = 1 ;i<= rows ;i++ ){
+
+        //     if( i == 1 || i == rows){
+        //         for(int j = 1 ; j<= columns ;j++){
+        //             System.out.print("# ");
+        //         }
+        //    }else{
+        //         for(int j = 1 ; j<= rows ;j++){
+        //             if (j ==1 || j == columns){
+        //                 System.out.print("# ");
+        //           }else{
+        //             System.out.print("  ");
+        //                  }
+        //                                         }
+        //            }
+        //     System.out.println("");
+
+        // }
 
     }
 
